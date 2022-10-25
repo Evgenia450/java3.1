6f789821d1b1734a7fe02c3e1b947c0f931775aa
Евгения Перова <merega89@mail.ru>
2022-10-26 00:03:57 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 5000;
+        int costPerMile = 20;
+        int mile;
+
+        mile = ticketPrice / costPerMile;
+        System.out.println("количество начисленных милей " + mile);
+    }
+}
